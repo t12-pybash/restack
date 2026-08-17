44cd7e048e0d84fee554f539ee577099ee0a7734
t12-pybash <pybashinf@gmail.com>
2026-08-17 13:16:06 +0100

    fix(ha): use allowUnfree instead of predicate for MongoDB

--- modules/ha/default.nix
+++ modules/ha/default.nix
@@ -20,7 +20,7 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "mongodb" ];
+    nixpkgs.config.allowUnfree = true;
 
     services.mongodb = {
       enable = true;
