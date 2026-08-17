c30d5abe0158fc48c9142646a4946423fefec50a
t12-pybash <pybashinf@gmail.com>
2026-08-17 13:14:14 +0100

    fix(ha): allow unfree for MongoDB (SSPL license)

--- modules/ha/default.nix
+++ modules/ha/default.nix
@@ -20,6 +20,8 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "mongodb" ];
+
     services.mongodb = {
       enable = true;
       extraConfig = ''
